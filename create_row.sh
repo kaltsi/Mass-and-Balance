f05300c014dd5df2c5f9e91db9ee2ec0d33bf67f
Juha Kallioinen <kaltsi+github@gmail.com>
2012-04-05 01:37:09 +0300

    Row code generator

---
+++ create_row.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# external
+g_code_line=""
+
+# This expects the input to be of the following format
+#
+# LP_I_xxx = interactive load point, declares a saveable variable
+# LP_F_xxx = interactive fuel flow row
+# LP_R_xxx = non-interactive load point with result and value cells reversed
+# LP_xxx   = non-interactive load point
+
+function create_row()
+{
+    if [ $# -lt 1 ]; then
+	echo "$FUNCNAME called with $*"
+	exit 1
+    fi
+
+    local lpname=$1
+    local reverse=""
+    local non=""
+
+    if [ "${1:0:5}" == "LP_R_" ]; then
+	reverse=", \"reverse\""
+    fi
+
+    if [[ "${1:0:5}" != "LP_I_" ]] && [[ "${1:0:5}" != "LP_F_" ]]; then
+	non="non_"
+    fi
+
+    g_code_line="rows.push(new ${non}interactive_row(${lpname})${reverse});"
+}
