c51c21835430402edf2ab0bd7ca0b62b622ac178
Juha Kallioinen <kaltsi+github@gmail.com>
2012-04-05 21:01:36 +0300

    The unit defaults need be given in LOAD_POINT definitions.

--- create_lp.sh
+++ create_lp.sh
@@ -4,9 +4,9 @@
 g_code_line=""
 g_saveable=""
 
-# name     translation       unit min max mom   [step]
+# name     translation       unit def min max mom   [step]
 #
-# LP_N_BEW "English" "Suomi" "kg" 0   -1  1.923  0.1
+# LP_N_BEW "English" "Suomi" "kg" 0   0   -1  1.923  0.1
 #
 # LP_I_xxx = interactive load point, declares a saveable variable
 # LP_F_xxx = interactive fuel flow row
@@ -15,7 +15,7 @@ g_saveable=""
 
 function create_lp()
 {
-    if [ $# -lt 7 ]; then
+    if [ $# -lt 8 ]; then
 	echo "$FUNCNAME called with $*"
 	exit 1
     fi
@@ -24,13 +24,14 @@ function create_lp()
     local lang_en=$2
     local lang_fi=$3
     local unit=$4
-    local unit_min=$5
-    local unit_max=$6
-    local mom=$7
+    local unit_def=$5
+    local unit_min=$6
+    local unit_max=$7
+    local mom=$8
     local steps=""
 
-    if [ $# -eq 8 ]; then
-	steps=$8
+    if [ $# -eq 9 ]; then
+	steps=$9
     fi
 
     local save_item=""
@@ -47,7 +48,15 @@ function create_lp()
     g_code_line="$g_code_line [$lang_en, $lang_fi],"
     g_code_line="$g_code_line ${unit},"
 
-    g_code_line="$g_code_line g_defs${save_item}.${save_name}, ${unit_min}, ${unit_max}, ${mom}"
+    # For non-interactive elements put the default value in place.
+    # For interactive values put the saveable name in place.
+    if [ "${lpname:0:5}" == "LP_N_" ] || [ "${lpname:0:5}" == "LP_R_" ]; then
+	g_code_line="$g_code_line ${unit_def},"
+    else
+	g_code_line="$g_code_line g_defs${save_item}.${save_name},"
+    fi
+
+    g_code_line="$g_code_line ${unit_min}, ${unit_max}, ${mom}"
 
     if [ ! -z ${steps} ]; then
 	g_code_line="$g_code_line, $steps"
@@ -60,9 +69,9 @@ function create_lp()
 	g_code_line="$g_code_line ${lpname}.fuel_flow = true;"
     fi
 
-    g_saveable="$save_name"
+    g_saveable="$save_name : ${unit_def},"
 }
 
-#create_lp LP_N_BEW "English" "Suomi" "kg" 0   -1  1.923  0.1
-#echo $code_line
+#create_lp LP_N_BEW "English" "Suomi" "kg" 0 0 -1  1.923 0.1
+#echo $g_code_line
 
